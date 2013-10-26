23a92c9ffc702ed916878f564e664d5f440fa118
ameerh90 <ameer.haider@tkxel.com>
2013-10-26 21:12:31 +0500

    updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   private
-  def after_sign_in_path_for(resource_or_scope)
-    users_path
+  def after_sign_in_path_for(resource)
+  	if resource.sign_in_count == 1 && resource.role != "admin"
+  		flash[:notice] = "You must change your password"
+  	    password_user_path(resource)
+  	else	
+    	 users_path
+    end
   end
 
 end
