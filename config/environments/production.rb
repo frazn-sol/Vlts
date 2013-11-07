60da78be4189f69cf17510583865b808841f6ffb
ameerh90 <ameer.haider@tkxel.com>
2013-11-07 22:27:09 +0500

    Up to date

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,7 @@ VLTS::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.assets.precompile += ['rollover.js', 'highcharts.js']
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
