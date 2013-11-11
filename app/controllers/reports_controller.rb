f7f4ac3747b49a61f8626989100904bbf727fb42
ameerh90 <ameer.haider@tkxel.com>
2013-11-11 20:43:35 +0500

    up to date

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -61,8 +61,7 @@ class ReportsController < ApplicationController
 			f.options[:xAxis][:categories] = ["Users"]
 
 			@user.each do |user|
-				binding.pry
-				support[user.first_name] = Customer.where(:user_id => "#{user.id}").count						
+				support[user.email] = Customer.where(:user_id => "#{user.id}").count						
 			end
 			support.each {|key, value|
 				f.series(:type=> 'column',:name=> key, :data=> [value], pointWidth: 50)
