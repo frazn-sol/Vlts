baf26f9fb2a1fcf2b22a9c42fa2b8c13aa56600d
ameerh90 <ameer.haider@tkxel.com>
2013-11-11 22:18:59 +0500

    up to date

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
 
   private
   def after_sign_in_path_for(resource)
-    binding.pry
   	if resource.pass_change == false && resource.role != "admin"
   		flash[:notice] = "You must change your password"
   		password_user_path(resource) 
