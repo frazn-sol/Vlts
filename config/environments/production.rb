ab002d1a6ea60607e40c5af960d9fa8461f7533d
ameerh90 <ameer.haider@tkxel.com>
2013-11-12 18:49:48 +0500

    up to date

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,14 +24,15 @@ VLTS::Application.configure do
   config.action_mailer.default_url_options = { :host => APP_HOST,}
   config.action_mailer.perform_deliveries  = true
   config.active_support.deprecation        = :notify
-  config.action_mailer.delivery_method     = :smtp
+  config.action_mailer.delivery_method     = :sendmail
   config.action_mailer.smtp_settings = {
     :address              => 'smtp.gmail.com',
     :port                 => 587,
     :domain               => 'gmail.com',
     :user_name            => "no.reply.vlts@gmail.com",
     :password             => 'imgreat1',
-    :authentication       => 'login'
+    :authentication       => 'login',
+    :enable_starttls_auto => true
   }
 
   # Defaults to Rails.root.join("public/assets")
