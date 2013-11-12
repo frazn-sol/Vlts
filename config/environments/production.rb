c59b8f94ba9d66df65a190849d2eef2d109e032a
ameerh90 <ameer.haider@tkxel.com>
2013-11-12 18:56:05 +0500

    up to date

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ VLTS::Application.configure do
   config.action_mailer.default_url_options = { :host => APP_HOST,}
   config.action_mailer.perform_deliveries  = true
   config.active_support.deprecation        = :notify
-  config.action_mailer.delivery_method     = :sendmail
+  config.action_mailer.delivery_method     = :smtp
   config.action_mailer.smtp_settings = {
     :address              => 'smtp.gmail.com',
     :port                 => 587,
