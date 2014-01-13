fc3c4cb5a1c71c0eee2eaf6391e8f6357e66cebe
ameerh90 <ameer.haider@tkxel.com>
2014-01-13 17:32:27 +0500

    up to date

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,5 +2,5 @@ class Vehicle < ActiveRecord::Base
 	audited
 	belongs_to :organization
 	has_many :vehicle_histories
-  attr_accessible :badge_number, :driver_first_name, :driver_last_name, :driver_middle_name, :expiry_date, :permit_date, :platenumber, :visitor, :organization_id, :colour, :make, :model
+  attr_accessible :badge_number, :driver_first_name, :driver_last_name, :driver_middle_name, :expiry_date, :permit_date, :platenumber, :visitor, :organization_id, :make, :model, :colour
 end
